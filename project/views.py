0443310536bec61baae5105d4e949bb5849d7bfc
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-07-05 12:01:22 +0200

    urls and views updated

--- project/views.py
+++ project/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def project(request, pk):
+    return render(request, 'project/project.html')
+
+def projects(request):
+    return render(request, 'project/projects.html')
