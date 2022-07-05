bf848c3051136a42466c439dbf361abfe9766aca
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-07-05 17:40:30 +0200

    Model forms

---
+++ project/forms.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from pyexpat import model
+from django.forms import ModelForm
+from .models import Project
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = '__all__'
