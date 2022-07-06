134466a30820479ae3f6730af4d3d2ac73695753
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-07-06 15:54:44 +0200

    changed the upload folder.
    Profile model

--- project/models.py
+++ project/models.py
@@ -7,7 +7,7 @@ class Project(models.Model):
     title = models.CharField(null=False, blank=False, max_length=200)
     description = models.TextField(null=True, blank=True)
     #owner
-    featured_image = models.ImageField(null=True, blank=True, default='Design.JPG', upload_to='static/images')
+    featured_image = models.ImageField(null=True, blank=True, default='Design.JPG', upload_to='static/images/')
     demo_link = models.CharField(null=True, blank=True, max_length=200)
     source_code = models.CharField(null=True, blank=True, max_length=200)
     created = models.DateTimeField(auto_now_add=True)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+import email
 from django.db import models
+from project.models import Project
+from django.contrib.auth.models import User 
+import uuid
 
 # Create your models here.
+class Profile(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, null=False, blank=False, primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=500, null=True, blank=True)
+    short_intro = models.CharField(max_length=200, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    profile_image = models.ImageField(null=True, blank=True, upload_to='static/images/profiles/')
