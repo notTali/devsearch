45ae12a5373e7598326622ba33794dd0553fa332
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-07-06 14:18:00 +0200

    User App is working well

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('projects/', views.projects, name='projects'),
@@ -10,5 +9,3 @@ urlpatterns = [
     path('update-project/<str:pk>/', views.updateProject, name='update-project'),
     path('delete-project/<str:pk>/', views.deleteProject, name='delete-project'),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
