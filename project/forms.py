30ea2b840d25003903dfdb5db0a1d8d630f358a0
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-07-08 13:52:33 +0200

    remove unused code

--- project/forms.py
+++ project/forms.py
@@ -7,16 +7,13 @@ class ProjectForm(ModelForm):
         model = Project
         fields = '__all__'
         widgets = {
-            'title':forms.TextInput(attrs={'class':'input form-control', 'placeholder':'Add Title'}),
-            'description':forms.Textarea(attrs={'class':'input form-control', 'placeholder':'Add Description'}),
+            'title':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Add Title'}),
+            'description':forms.Textarea(attrs={'class':'form-control', 'placeholder':'Add Description'}),
             'tags':forms.SelectMultiple(attrs={'class':'form-select'}),
             'owner':forms.Select(attrs={'class':'form-select'}),
-            'featured_image':forms.FileInput(attrs={'class':'form-control form-control-lg'}), #form-control form-control-lg
-            'demo_link':forms.TextInput(attrs={'class':'input form-control', 'placeholder':'Add Demo Link'}),
-            'source_code':forms.TextInput(attrs={'class':'input form-control', 'placeholder':'Add Source Link'}),
+            # 'featured_image':forms.FileInput(attrs={'class':'form-control form-control-lg'}), #form-control form-control-lg
+            'demo_link':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Add Demo Link'}),
+            'source_code':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Add Source Link'}),
         }
 
-        # def __init__(self, *args, **kwargs):
-        #     super(ProjectForm, self).__init__(*args, **kwargs)
-
-        #     self.fields['title'].widget.attrs.update({'class':'input', 'placeholder':'Add Text'})
+        
